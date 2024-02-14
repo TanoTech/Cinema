c20355743c5215588b744d8fa0ee042c505da416
Gaetano <gaetano.napoli@hotmail.com>
2024-02-14 18:05:55 +0100

    prova push da terminale

--- Prenotazioni.aspx.designer.cs
+++ Prenotazioni.aspx.designer.cs
@@ -75,6 +75,6 @@ namespace Cinema
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.PlaceHolder phPrenotazioni;
+        protected global::System.Web.UI.WebControls.Label phPrenotazioni;
     }
 }
