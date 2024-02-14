d706c151f6295ca4b2928a1d499d701a26ab15dd
gaetano <gaetano@GAETANO858A>
2024-02-14 16:45:02 +0100

    Add project files.

---
+++ Prenotazioni.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Prenotazioni.aspx.cs" Inherits="Cinema.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+  <title>Prenotazioni</title>
+</head>
+<body>
+  <form id="form1" runat="server">
+ <div>
+  <label for="txtNome">Nome:</label>
+  <asp:TextBox ID="txtNome" runat="server" Required="true" />
+
+  <label for="txtCognome">Cognome:</label>
+  <asp:TextBox ID="txtCognome" runat="server" Required="true" />
+
+  <label for="ddlSala">Sala:</label>
+  <asp:DropDownList ID="ddlSala" runat="server">
+  </asp:DropDownList>
+
+  <label for="chkPrezzoRidotto">Prezzo Ridotto:</label>
+  <asp:CheckBox ID="chkPrezzoRidotto" runat="server"/>
+
+  <asp:Button ID="btnPrenota" runat="server" Text="Prenota" OnClick="btnPrenota_Click" />
+
+
+ </div>
+
+<asp:PlaceHolder ID="phPrenotazioni" runat="server"></asp:PlaceHolder>
+
+</form>
+
+</body>
+</html>
---
+++ Prenotazioni.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Cinema
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CaricaSaleCinema();
+                VisualizzaPrenotazioni();
+            }
+        }
+
+        private void CaricaSaleCinema()
+        {
+            ddlSala.Items.Add(new ListItem("Sala Est", "SalaEst"));
+            ddlSala.Items.Add(new ListItem("Sala Sud", "SalaSud"));
+            ddlSala.Items.Add(new ListItem("Sala Nord", "SalaNord"));
+        }
+
+        protected void btnPrenota_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            string cognome = txtCognome.Text;
+            string salaSelezionata = ddlSala.SelectedValue;
+            bool ridotto = chkPrezzoRidotto.Checked;
+
+            string nomeTabella = $"dbo.[{salaSelezionata}]";
+
+            SalvaPrenotazione(nomeTabella, nome, cognome, ridotto);
+            VisualizzaPrenotazioni();
+        }
+
+        private void SalvaPrenotazione(string nomeTabella, string nome, string cognome, bool ridotto)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString;
+
+            string query = $"INSERT INTO {nomeTabella} (Nome, Cognome, Ridotto) VALUES (@Nome, @Cognome, @Ridotto)";
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@Nome", nome);
+                    cmd.Parameters.AddWithValue("@Cognome", cognome);
+                    cmd.Parameters.AddWithValue("@Ridotto", ridotto);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void VisualizzaPrenotazioni()
+        {
+            phPrenotazioni.Controls.Clear();
+
+            foreach (string sala in new string[] { "SalaEst", "SalaSud", "SalaNord" })
+            {
+
+                Label lblSala = new Label();
+                lblSala.Text = $"Prenotazioni per {sala}:";
+                phPrenotazioni.Controls.Add(lblSala);
+
+                string connectionString = ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString;
+                string query = $"SELECT Nome, Cognome, Ridotto FROM {sala}";
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        con.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string nome = reader.GetString(0);
+                                string cognome = reader.GetString(1);
+                                bool ridotto = reader.GetBoolean(2);
+
+                                Label lblPrenotazione = new Label();
+                                lblPrenotazione.Text = $"{nome} {cognome} - Prezzo ridotto: {ridotto}";
+                                phPrenotazioni.Controls.Add(lblPrenotazione);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
